3c5dbfe7a1546e7aa7944c61373aacd213ebdcd0
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-10-28 14:44:01 -0400

    formatting updates

--- index.js
+++ index.js
@@ -86,15 +86,14 @@ switch (weather) {
 		console.log("Unknown weather");
 }
 
+// object orientated javascipt 
 function Rabbit(type) {
 	this.type = type;
 }
-
 Rabbit.prototype.speak = function(line) {
 	console.log(`The ${this.type} rabbit says ${line}`)
 }
 
 let weirdRabbit = new Rabbit("weird");
-
 weirdRabbit.speak("hello");
 	
